81fb3437392c8df50e2214950dfd3276d73308b4
Inamullah <sheenzada7@gmail.com>
2026-06-11 08:05:08 -0700

    c++

---
+++ Heart.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+
+int main () {
+    float x,y,a;
+    for(y=1.5; y >-1.5;y=0.1){
+        for(x=1.5;x<1.5; x += 0.05){
+            a=x*x+y*y-1;
+            putchar(a*a*a-x*x*y*y*y<=0.0?'*':' ');
+        }
+        system("color 0c")
+        putchar('\n');
+    }
+    printf("i love you <3");
+    return 0;
+}
